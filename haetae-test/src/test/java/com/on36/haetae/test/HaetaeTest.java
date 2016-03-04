f9a23c86f4303f9dce084044fc906d5e478b14dd
on36 <273330479@qq.com>
2016-03-04 14:26:56 +0800

    commit

--- haetae-test/src/test/java/com/on36/haetae/test/HaetaeTest.java
+++ haetae-test/src/test/java/com/on36/haetae/test/HaetaeTest.java
@@ -15,10 +15,83 @@ import com.ning.http.multipart.StringPart;
 public class HaetaeTest {
 
 	@Test
+	public void testHello() throws Exception {
+		AsyncHttpClient asyncHttpClient = new AsyncHttpClient();
+		Response resp = asyncHttpClient
+				.prepareGet("http://localhost:8080/services/hello")
+				.execute().get();
+		
+		String result = resp.getResponseBody().trim();
+		System.out.println(result);
+		Assert.assertEquals("Hello xiongdi!", result);
+		asyncHttpClient.close();
+	}
+	@Test
+	public void testName() throws Exception {
+		AsyncHttpClient asyncHttpClient = new AsyncHttpClient();
+		Response resp = asyncHttpClient
+				.prepareGet("http://localhost:8080/services/name/zhangsan")
+				.execute().get();
+		
+		String result = resp.getResponseBody().trim();
+		System.out.println(result);
+		Assert.assertEquals("Hello zhangsan", result);
+		asyncHttpClient.close();
+	}
+	@Test
+	public void testMulti() throws Exception {
+		AsyncHttpClient asyncHttpClient = new AsyncHttpClient();
+		Response resp = asyncHttpClient
+				.prepareGet("http://localhost:8080/services/multi/zhangsan/123")
+				.execute().get();
+		
+		String result = resp.getResponseBody().trim();
+		System.out.println(result);
+		Assert.assertEquals("Hello zhangsan 123", result);
+		asyncHttpClient.close();
+	}
+	@Test
+	public void testHeaderValue() throws Exception {
+		AsyncHttpClient asyncHttpClient = new AsyncHttpClient();
+		Response resp = asyncHttpClient
+				.prepareGet("http://localhost:8080/services/greeting")
+				.execute().get();
+		
+		String result = resp.getResponseBody().trim();
+		System.out.println(result);
+		Assert.assertEquals("Hello NING/1.0", result);
+		asyncHttpClient.close();
+	}
+	@Test
+	public void testBlackList() throws Exception {
+		AsyncHttpClient asyncHttpClient = new AsyncHttpClient();
+		Response resp = asyncHttpClient
+				.prepareGet("http://localhost:8080/services/black")
+				.execute().get();
+		
+		String result = resp.getResponseBody().trim();
+		System.out.println(result);
+		Assert.assertEquals("Hello black!", result);
+		asyncHttpClient.close();
+	}
+	@Test
+	public void testWhiteList() throws Exception {
+		AsyncHttpClient asyncHttpClient = new AsyncHttpClient();
+		Response resp = asyncHttpClient
+				.prepareGet("http://localhost:8080/services/white")
+				.execute().get();
+		
+		String result = resp.getResponseBody().trim();
+		System.out.println(result);
+		Assert.assertEquals("Hello white!", result);
+		asyncHttpClient.close();
+	}
+	@Test
 	public void testBodyParts() throws Exception {
 		AsyncHttpClient asyncHttpClient = new AsyncHttpClient();
 		Response resp = asyncHttpClient
 				.preparePost("http://localhost:8080/services/custom")
+				.addBodyPart(new StringPart("user", "zhangsan"))
 				.addBodyPart(new StringPart("name", "nihao"))
 				.execute().get();
 		
@@ -32,19 +105,20 @@ public class HaetaeTest {
 		AsyncHttpClient asyncHttpClient = new AsyncHttpClient();
 		Response resp = asyncHttpClient
 				.preparePost("http://localhost:8080/services/custom")
-				.addParameter("name", "wangwu")
+				.addParameter("user", "zhangsan")
+				.addParameter("name", "nihao")
 				.execute().get();
 		
 		String result = resp.getResponseBody().trim();
 		System.out.println(result);
-		Assert.assertEquals("wangwu", result);
+		Assert.assertEquals("zhangsan", result);
 		asyncHttpClient.close();
 	}
 	@Test
 	public void testSplatParameter() throws Exception {
 		AsyncHttpClient asyncHttpClient = new AsyncHttpClient();
 		Response resp = asyncHttpClient
-				.preparePost("http://localhost:8080/services/custom?name=zhangsan")
+				.preparePost("http://localhost:8080/services/custom?user=zhangsan&name=nihao")
 				.execute().get();
 		
 		String result = resp.getResponseBody().trim();
