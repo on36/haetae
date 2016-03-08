c925e9f483ffa491a2649e440c8003eca152390a
on36 <273330479@qq.com>
2016-03-08 15:29:50 +0800

    commit

---
+++ haetae-server/src/main/java/com/on36/haetae/server/core/body/TimeoutResponseBody.java
@@ -0,0 +1,28 @@
+package com.on36.haetae.server.core.body;
+
+import io.netty.handler.codec.http.HttpResponse;
+import io.netty.handler.codec.http.HttpResponseStatus;
+
+/**
+ * @author zhanghr
+ * @date 2016年1月8日
+ */
+public class TimeoutResponseBody extends StringResponseBody {
+
+	public TimeoutResponseBody() {
+		super("request time out");
+	}
+	
+	@Override
+	public boolean hasContent() {
+		// TODO Auto-generated method stub
+		return true;
+	}
+
+	@Override
+	public void sendAndCommit(HttpResponse response, String contentType) {
+		response.setStatus(HttpResponseStatus.BAD_REQUEST);
+		super.sendAndCommit(response, contentType);
+	}
+
+}
