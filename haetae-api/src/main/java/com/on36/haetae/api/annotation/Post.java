5e0eba0dbd70e08db1b95e77f284974ad054a0ca
on36 <273330479@qq.com>
2016-03-10 17:57:07 +0800

    commit

---
+++ haetae-api/src/main/java/com/on36/haetae/api/annotation/Post.java
@@ -0,0 +1,17 @@
+package com.on36.haetae.api.annotation;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+/**
+ * @author zhanghr
+ * @date 2016年1月9日
+ */
+
+@Retention(RetentionPolicy.RUNTIME)
+@Target(ElementType.METHOD)
+public @interface Post {
+
+}
