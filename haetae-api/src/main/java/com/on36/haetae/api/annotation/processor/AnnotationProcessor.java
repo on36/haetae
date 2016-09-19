fc21057de8185d3da40e43bfe0b17bac42e672a7
on36 <273330479@qq.com>
2016-09-19 16:17:52 +0800

    add annotation processor

---
+++ haetae-api/src/main/java/com/on36/haetae/api/annotation/processor/AnnotationProcessor.java
@@ -0,0 +1,45 @@
+package com.on36.haetae.api.annotation.processor;
+
+import java.util.Set;
+
+import javax.annotation.processing.AbstractProcessor;
+import javax.annotation.processing.Messager;
+import javax.annotation.processing.RoundEnvironment;
+import javax.annotation.processing.SupportedAnnotationTypes;
+import javax.annotation.processing.SupportedSourceVersion;
+import javax.lang.model.SourceVersion;
+import javax.lang.model.element.Element;
+import javax.lang.model.element.Modifier;
+import javax.lang.model.element.TypeElement;
+import javax.tools.Diagnostic;
+
+import com.on36.haetae.api.annotation.ApiDoc;
+
+/**
+ * @author zhanghr
+ * @date 2016年3月16日
+ */
+@SupportedAnnotationTypes({ "com.on36.haetae.api.annotation.Api" })
+@SupportedSourceVersion(SourceVersion.RELEASE_8)
+public class AnnotationProcessor extends AbstractProcessor {
+	public boolean process(Set<? extends TypeElement> annotations,
+			RoundEnvironment env) {
+		Messager messager = processingEnv.getMessager();
+		for (TypeElement te : annotations) {
+			for (Element e : env.getElementsAnnotatedWith(te)) {
+				if (!e.getModifiers().contains(Modifier.PUBLIC)) {
+					messager.printMessage(Diagnostic.Kind.ERROR,
+							"The method that be defined by the annotation @Api must be public",
+							e);
+				}
+
+				ApiDoc apiDoc = e.getAnnotation(ApiDoc.class);
+				if (apiDoc == null)
+					messager.printMessage(Diagnostic.Kind.WARNING,
+							"The annotation @ApiDoc must be defined at the method that be defined by the annotation @Api",
+							e);
+			}
+		}
+		return false;
+	}
+}
