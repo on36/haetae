c793f4fa02f9983ccab827652f7b262d6d7dec29
on36 <273330479@qq.com>
2016-10-19 14:32:10 +0800

    Fix query part

--- haetae-client/src/main/java/com/on36/haetae/config/client/ServiceClient.java
+++ haetae-client/src/main/java/com/on36/haetae/config/client/ServiceClient.java
@@ -1,5 +1,7 @@
 package com.on36.haetae.config.client;
 
+import java.util.HashMap;
+
 /**
  * @author zhanghr
  * @date 2016年5月13日
@@ -26,8 +28,8 @@ public class ServiceClient {
 		if (address == null)
 			return false;
 		try {
-			HttpClient.getInstance()
-					.post(getURI("/service/register?address=" + address));
+			HttpClient.getInstance().post(getURI("/service/register"),
+					new QueryPart("address", address));
 			return true;
 		} catch (Exception e) {
 			e.printStackTrace();
@@ -45,8 +47,8 @@ public class ServiceClient {
 			throws Exception {
 		if (address == null)
 			return false;
-		HttpClient.getInstance()
-				.post(getURI("/node?address=" + address + "&data=" + data));
+		HttpClient.getInstance().post(getURI("/node"),
+				new QueryPart("node", address).addQueryPart("data", data));
 		return true;
 	}
 
@@ -60,11 +62,26 @@ public class ServiceClient {
 		if (address == null)
 			return false;
 		try {
-			HttpClient.getInstance().delete(getURI("/node?address=" + address));
+			HttpClient.getInstance().delete(getURI("/node"),
+					new QueryPart("node", address));
 			return true;
 		} catch (Exception e) {
 			e.printStackTrace();
 		}
 		return false;
+	}
+
+	static class QueryPart extends HashMap<String, String> {
+
+		private static final long serialVersionUID = 1L;
+
+		public QueryPart(String key, String value) {
+			put(key, value);
+		}
+
+		public QueryPart addQueryPart(String key, String value) {
+			put(key, value);
+			return this;
+		}
 	}
 }
